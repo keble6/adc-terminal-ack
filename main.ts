daaa9ecb4acd9f7194e7d7f7c3136e6ffd0f186d
Rob Wilson <rob@rwilson.org.uk>
2022-09-09 08:43:56 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+radio.onReceivedString(function (receivedString) {
+    // Send only completed messages (i.e. does not end in comma)
+    if (receivedString.charAt(receivedString.length - 1).compare(",") == 0) {
+        outString = "" + outString + receivedString
+    } else {
+        outString = "" + outString + receivedString
+        serial.writeLine(outString)
+        // This is used by the sender - it has to receive ack before sending next data
+        radio.sendString("ack")
+        outString = ""
+    }
+})
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    serialString = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    radio.sendString(serialString)
+})
+let serialString = ""
+let outString = ""
+radio.setGroup(1)
+radio.setTransmitPower(7)
+outString = ""
